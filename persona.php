dfe500045cb1eef310b0029aa7132069307969c3
Paulo Galarza <paulo-123@hotmail.es>
2024-08-17 14:39:14 -0500

    first commit

---
+++ persona.php
@@ -0,0 +1,20 @@
+class Persona{
+$colorCabello;
+$altura;
+$peso;
+$coloOjos;
+$colorPiel;
+$edad;
+function trabajar(){
+echo "Voy a trabajar";
+}
+function comer(){
+echo "Voy a comer";
+}
+function dormir(){
+echo "Voy a dormir";
+}
+function caminar(){
+echo "Voy a caminar";
+}
+}
